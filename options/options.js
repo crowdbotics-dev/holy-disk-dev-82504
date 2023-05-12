c48c71d8540fecda3ff40b71a5fb524a94d40789
Crowdbotics <engineering@crowdbotics.com>
2023-05-12 01:22:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-disk-dev-82504",
+  url: "https://holy-disk-dev-82504.botics.co",
+  api: "https://holy-disk-dev-82504.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
